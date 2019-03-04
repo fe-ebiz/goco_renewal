e3fd55d692ece5812aa80dc554e8b767818d1279
joon <vxggangxv@naver.com>
2019-03-04 14:47:11 +0900

    모바일 메인 제작완료

--- dist/static/js/renew/m/common.js
+++ dist/static/js/renew/m/common.js
@@ -1,3 +1,21 @@
 $(function() {
 	
+	footerFn();
+	bnbFn();
+	
 });
+
+// 푸터 기능
+function footerFn() {
+	$(".rn-footer .address .fold").on("click", function() {
+		$(this).toggleClass("on");
+		$(this).next().slideToggle();
+	});
+}
+
+// 하단 고정 메뉴 기능
+function bnbFn() {
+	$(".bnb .menu > li a").on("click", function() {
+		$(this).closest("li").toggleClass("on");
+	});
+}
